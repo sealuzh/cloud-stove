d6a6664270d3e98c7f09c529af78ca657bbeb4a7
Christian Inzinger <christian@inz.io>
2015-11-17 16:18:44 +0100

    Fix typo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require boostrap-sprockets
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
