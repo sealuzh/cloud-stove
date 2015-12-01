045478af3158d2692a9047d6855fe3373d3463c0
Christian Inzinger <christian@inz.io>
2015-12-01 12:30:53 +0100

    Add DigitalOcean resource fetcher
    Stores both, per-minute and per-hour prices. Ignores regions.

---
+++ app/provider_updaters/digital_ocean_updater.rb
@@ -0,0 +1,30 @@
+class DigitalOceanUpdater < ProviderUpdater
+  def perform
+    token = ENV['DIGITALOCEAN_TOKEN']
+    uri = URI("https://api.digitalocean.com/v2/sizes")
+    
+    request = Net::HTTP::Get.new(uri)
+    request['Authorization']="Bearer #{token}"
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') { |http| http.request(request) }
+    response.value
+    
+    pricelist = JSON.parse(response.body)
+    provider = Provider.find_or_create_by(name: 'Digital Ocean')
+    provider.more_attributes['pricelist'] = pricelist
+    provider.save!
+    
+    pricelist['sizes'].each do |instance_type|
+      resource_id = instance_type['slug']
+      resource = provider.resources.find_or_create_by(name: resource_id)
+      
+      resource.more_attributes['type'] = 'compute'
+      resource.more_attributes['cores'] = instance_type['vcpus']
+      resource.more_attributes['mem_gb'] = BigDecimal.new(instance_type['memory']) / 1024
+      resource.more_attributes['price_per_hour'] = instance_type['price_hourly']
+      resource.more_attributes['price_per_month'] = instance_type['price_monthly']
+      resource.save!
+    end
+  rescue Net::HTTPError, JSON::ParserError => e
+    logger.error "Error, #{e.inspect}"
+  end
+end
