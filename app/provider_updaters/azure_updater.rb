5dd2752f34083fa4d668255e3a3c771c775f56a6
Christian Inzinger <christian@inz.io>
2015-12-07 15:02:08 +0100

    Add rudimentary Azure price fetcher

---
+++ app/provider_updaters/azure_updater.rb
@@ -0,0 +1,41 @@
+class AzureUpdater < ProviderUpdater
+  def perform
+    uri = URI('https://azure.microsoft.com/en-us/pricing/details/virtual-machines/')
+
+    doc = Nokogiri::HTML(open(uri))
+
+    pricelist = {}
+
+    pricelist_pos = doc.css('[data-tab-panel="tab-panel-os"]').css('[data-href="#Linux"]').attribute('data-id').text
+    pricelist_div = doc.css('[data-tab-panel-id="tab-panel-os"]').css("div:eq(#{pricelist_pos})")
+    pricelist_entries = pricelist_div.css("table > tbody > tr")
+
+    pricelist_entries.each do |tr|
+      cells = tr.css('td')
+      # Pricing table rows have 5 cells:
+      # instance id, cores, ram, disk sizes, price
+      next if cells.count < 5
+
+      resource_id = cells.first.text.gsub(/\s/, '')
+      price_per_hour = cells[4].css('[data-condition-region="us-east"] .price-data').attribute('data-amount').to_s
+
+      pricelist[resource_id] = {
+        'type' => 'compute',
+        'cores' => cells[1].css('strong').text,
+        'mem_gb' => cells[2].css('strong').text,
+        'price_per_hour' => price_per_hour,
+      }
+    end
+    
+    provider = Provider.find_or_create_by(name: 'Microsoft Azure')
+    provider.more_attributes['pricelist'] = pricelist
+    provider.more_attributes['sla'] = extract_sla('https://azure.microsoft.com/en-us/support/legal/sla/virtual-machines/v1_0/')
+    provider.save!
+    
+    pricelist.each_pair do |resource_id, data|
+      resource = provider.resources.find_or_create_by(name: resource_id)
+      resource.more_attributes = data
+      resource.save!
+    end
+  end
+end
