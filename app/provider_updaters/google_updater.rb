b3cb8cae3e4f20c0d0f3565fdfba0132321e8e9d
Christian Inzinger <christian@inz.io>
2015-12-07 15:00:43 +0100

    Add sustained use discount for Google pricing
    For now, will only respect discounts in monthly pricing (not depending on the number of hours)

--- app/provider_updaters/google_updater.rb
+++ app/provider_updaters/google_updater.rb
@@ -13,9 +13,16 @@ class GoogleUpdater < ProviderUpdater
     provider.more_attributes['sla'] = extract_sla('https://cloud.google.com/compute/sla')
     provider.save!
     
+    gcp_price_list = pricelist['gcp_price_list']
+    sustained_base = BigDecimal.new(gcp_price_list['sustained_use_base'].to_s)
+    sustained_tier_values = gcp_price_list['sustained_use_tiers'].values
+    full_month_discount = sustained_tier_values.inject(0) do |sum, n| 
+      sum + BigDecimal.new(n.to_s) * sustained_base 
+    end
+    
     gce_prefix = 'cp-computeengine-vmimage-'
     preemptible_postfix = '-preemptible'
-    pricelist['gcp_price_list'].each_pair do |key, value|
+    gcp_price_list.each_pair do |key, value|
       resource_id = key.downcase
       next unless resource_id.start_with? gce_prefix
       preemptible = resource_id.end_with?(preemptible_postfix)
@@ -24,7 +31,13 @@ class GoogleUpdater < ProviderUpdater
       next if preemptible
       resource = provider.resources.find_or_create_by(name: resource_id)
       resource.more_attributes['type'] = 'compute'
-      resource.more_attributes['price_per_hour'] = value['us']
+      # FIXME: For now, use prices for instances in Europe. They're more
+      # expensive. Better for conservative estimates. Should eventually
+      # include all regions
+      resource.more_attributes['price_per_hour'] = value['europe']
+      # FIXME: 744 hours/month assumes a 31 day month. Same as above.
+      price_per_month = BigDecimal.new(value['europe'].to_s) * 744 * full_month_discount
+      resource.more_attributes['price_per_month'] = price_per_month
       resource.more_attributes['cores'] = value['cores']
       resource.more_attributes['mem_gb'] = value['memory']
       resource.save!
