7a1bf00c76ff3e932d7d8cff91c0092fcae807d1
Christian Inzinger <christian@inz.io>
2015-12-07 16:37:16 +0100

    Assume low CPU budget for shared cores

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,6 +1,6 @@
 module ResourcesHelper
   def price_per_core(resource)
-    cores = resource.ma['cores'] == 'shared' ? 1 : resource.ma['cores'].to_s
+    cores = resource.ma['cores'] == 'shared' ? 0.1 : resource.ma['cores']
     number_with_precision(resource.price_per_month / BigDecimal.new(cores.to_s), precision: 3)
   end
   
