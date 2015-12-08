1dcdde3413787fbe76bd989d9c105a59506360ce
Christian Inzinger <christian@inz.io>
2015-12-08 14:27:12 +0100

    More consistent price formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def format_price(price, precision: 2)
+    number_to_currency(price, precision: precision)
+  end
 end
--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,10 +1,12 @@
 module ResourcesHelper
-  def price_per_core(resource)
+  def price_per_core(resource, raw: false)
     cores = resource.ma['cores'] == 'shared' ? 0.1 : resource.ma['cores']
-    number_with_precision(resource.price_per_month / BigDecimal.new(cores.to_s), precision: 3)
+    ppc = resource.price_per_month / BigDecimal.new(cores.to_s)
+    raw ? ppc : format_price(ppc)
   end
   
-  def price_per_gb_ram(resource)
-    number_with_precision(resource.price_per_month / BigDecimal.new(resource.ma['mem_gb'].to_s), precision: 3)
+  def price_per_gb_ram(resource, raw: false)
+    ppgr = resource.price_per_month / BigDecimal.new(resource.ma['mem_gb'].to_s)
+    raw ? ppgr : format_price(ppgr, precision: 2)
   end
 end
