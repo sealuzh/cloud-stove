7830410d1f02319a86ebcd6e196df83b8ea95898
Christian Inzinger <christian@inz.io>
2016-01-07 13:24:12 +0100

    Add link to update provider pricing data

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -2,4 +2,9 @@ class ProvidersController < ApplicationController
   def index
     @providers = Provider.includes(:resources)
   end
+  
+  def update_all
+    Provider.update_providers
+    redirect_to providers_path
+  end
 end
