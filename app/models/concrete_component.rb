78dbc831654b1aed34d75ebcc7a4d80756bd0301
Genc Mazlami <genc.mazlami@uzh.ch>
2016-02-04 18:05:28 +0100

    Slo: more_attributes are now correctly serialized.

--- app/models/concrete_component.rb
+++ app/models/concrete_component.rb
@@ -3,4 +3,16 @@ class ConcreteComponent < Base
   belongs_to :component
   has_many :slos
   accepts_nested_attributes_for :slos
+
+  def slos_attributes=(attributes)
+    slos = []
+    attributes.each do |key, value|
+      slo = Slo.new(value) unless value['_destroy'] == '1'
+      slo.more_attributes = ActiveSupport::JSON.decode(value['more_attributes'])
+      slos << slo
+    end
+    self.slos = slos
+    save!
+  end
+
 end
--- app/models/slo.rb
+++ app/models/slo.rb
@@ -1,6 +1,12 @@
 class Slo < Base
+  ma_accessor :value
+  ma_accessor :metric
+  ma_accessor :relation
 
+  attr_accessor :_destroy
   belongs_to :concrete_component
+
+
 
   def human_metric
     I18n.t(ma['metric'], scope: 'slos.metrics')
