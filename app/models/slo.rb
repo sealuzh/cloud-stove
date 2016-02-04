d0ba5446aea0b5b4714170dabf8004536fc322b1
Genc Mazlami <genc.mazlami@uzh.ch>
2016-02-04 19:48:44 +0100

    Slo: more_attributes is correctly deserialized.

--- app/models/slo.rb
+++ app/models/slo.rb
@@ -1,12 +1,12 @@
 class Slo < Base
-  ma_accessor :value
-  ma_accessor :metric
-  ma_accessor :relation
 
   attr_accessor :_destroy
   belongs_to :concrete_component
 
 
+  def more_attributes
+    read_attribute(:more_attributes).to_json
+  end
 
   def human_metric
     I18n.t(ma['metric'], scope: 'slos.metrics')
