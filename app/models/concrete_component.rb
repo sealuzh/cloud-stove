a3423260cbfea0f41b66342ec20e1d640bbda082
Genc Mazlami <genc.mazlami@uzh.ch>
2016-02-05 13:23:44 +0100

    Allow destroying nested model instances.

--- app/models/concrete_component.rb
+++ app/models/concrete_component.rb
@@ -2,7 +2,7 @@ class ConcreteComponent < Base
   ma_accessor :body
   belongs_to :component
   has_many :slos
-  accepts_nested_attributes_for :slos
+  accepts_nested_attributes_for :slos, allow_destroy: true
 
 
 end
