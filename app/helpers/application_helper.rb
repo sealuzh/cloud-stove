60aee6cecc737c4e41798d641bfecfe660323af9
Christian Inzinger <christian@inz.io>
2016-02-08 11:10:08 +0100

    Refactor form helpers because DRY
    `sub_form` is a proper subset of `link_to_add_fields` functionality.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,18 +11,13 @@ module ApplicationHelper
   
   # Adapted from https://github.com/railscasts/196-nested-model-form-revised/blob/master/questionnaire-after/app/helpers/application_helper.rb
   def link_to_add_fields(name, f, association, klass = association.to_s.camelize.constantize)
-    new_object = klass.new
-    id = new_object.object_id
-    fields = f.fields_for(association, new_object, child_index: id) do |builder|
-      render(association.to_s.singularize + "_fields", f: builder)
-    end
+    id = Time.now.to_i
+    fields = sub_form(f, association, klass, id)
     link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
   end
 
-  def sub_form(f, association, klass = association.to_s.camelize.constantize)
-    new_object = klass.new
-    id = new_object.object_id
-    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+  def sub_form(f, association, klass = association.to_s.camelize.constantize, id = Time.now.to_i)
+    fields = f.fields_for(association, klass.new, child_index: id) do |builder|
       render(association.to_s.singularize + "_fields", f: builder)
     end
   end
