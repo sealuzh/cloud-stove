4f75acc324857d1a2dd9714f4073d4ca7a8ed9f7
Christian Inzinger <christian@inz.io>
2016-02-10 18:20:55 +0100

    Blueprint, CloudApplication now destroy dependent components

--- app/models/blueprint.rb
+++ app/models/blueprint.rb
@@ -1,6 +1,6 @@
 class Blueprint < Base
   ma_accessor :body
-  has_many :components
+  has_many :components, dependent: :destroy
   has_many :cloud_applications
   accepts_nested_attributes_for :components, allow_destroy: true
 end
