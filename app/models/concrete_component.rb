7540dadcb681ea1262cc11fb1132b36ffd1e122a
Genc Mazlami <genc.mazlami@uzh.ch>
2016-02-11 23:29:39 +0100

    Add cloud_application accessor to ConcreteComponent

--- app/models/concrete_component.rb
+++ app/models/concrete_component.rb
@@ -1,6 +1,7 @@
 class ConcreteComponent < Base
   ma_accessor :body
   belongs_to :component
+  belongs_to :cloud_application
   has_many :slos
   accepts_nested_attributes_for :slos, allow_destroy: true
 
