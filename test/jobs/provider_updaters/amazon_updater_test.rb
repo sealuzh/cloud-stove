388455123576503c93cdb150d7110a58ff2b5324
Genc Mazlami <genc.mazlami@uzh.ch>
2016-03-10 11:17:10 +0100

    Extend test case and mocks for AmazonUpdater.

--- test/jobs/provider_updaters/amazon_updater_test.rb
+++ test/jobs/provider_updaters/amazon_updater_test.rb
@@ -2,10 +2,19 @@ require 'test_helper'
 
 class AmazonUpdaterTest < ActiveJob::TestCase
   setup do
+
+    #EC2 compute
     WebMock.stub_request(:get, 'https://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js').
         to_return(response_from('amazon-ec2-pricing.txt'))
     WebMock.stub_request(:get, 'https://aws.amazon.com/ec2/sla/').
         to_return(response_from('amazon-ec2-sla.txt'))
+
+    #S3 storage
+    WebMock.stub_request(:get, 'http://a0.awsstatic.com/pricing/1/s3/pricing-storage-s3.min.js').
+        to_return(response_from('amazon-s3-storage-pricing.txt'))
+    WebMock.stub_request(:get, 'https://aws.amazon.com/s3/sla/').
+        to_return(response_from('amazon-s3-sla.txt'))
+
   end
 
   test 'creates resources in db' do
@@ -15,7 +24,8 @@ class AmazonUpdaterTest < ActiveJob::TestCase
 
     provider = Provider.find_by(name: 'Amazon')
     assert_not_nil provider
-    assert_not_empty provider.resources
+    assert_not_empty provider.resources.where(resource_type: 'compute')
+    assert_not_empty provider.resources.where(resource_type: 'storage')
   end
 
 end
