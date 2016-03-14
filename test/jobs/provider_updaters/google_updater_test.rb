7a97b713ee56cd6db8b79aa5bc47146a3f85d018
Genc Mazlami <genc.mazlami@uzh.ch>
2016-03-14 15:32:08 +0100

    Add WebMock request for Google storage SLA.

--- test/jobs/provider_updaters/google_updater_test.rb
+++ test/jobs/provider_updaters/google_updater_test.rb
@@ -3,9 +3,13 @@ require 'test_helper'
 class GoogleUpdaterTest < ActiveJob::TestCase
   setup do
     WebMock.stub_request(:get, 'https://cloudpricingcalculator.appspot.com/static/data/pricelist.json').
-        to_return(response_from('google-compute-pricing.txt'))
+        to_return(response_from('google-pricing.txt'))
+
     WebMock.stub_request(:get, 'https://cloud.google.com/compute/sla').
         to_return(response_from('google-compute-sla.txt'))
+
+    WebMock.stub_request(:get, 'https://cloud.google.com/storage/sla').
+        to_return(response_from('google-storage-sla.txt'))
   end
 
   test 'creates resources in db' do
