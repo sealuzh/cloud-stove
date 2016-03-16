50f863963a0f4e443e37a332e98ee2e1aa2bd059
Genc Mazlami <genc.mazlami@uzh.ch>
2016-03-16 17:53:47 +0100

    Fix testcase for JoyentUpdater with storage resources.

--- test/jobs/provider_updaters/joyent_updater_test.rb
+++ test/jobs/provider_updaters/joyent_updater_test.rb
@@ -4,6 +4,10 @@ class JoyentUpdaterTest < ActiveJob::TestCase
   setup do
     WebMock.stub_request(:get, 'https://www.joyent.com/assets/js/pricing.json').
         to_return(response_from('joyent-compute-pricing.txt'))
+
+    WebMock.stub_request(:get, 'https://www.joyent.com/object-storage/pricing').
+        to_return(response_from('joyent-storage-pricing.txt'))
+
   end
 
   test 'creates resources in db' do
@@ -14,8 +18,13 @@ class JoyentUpdaterTest < ActiveJob::TestCase
     provider = Provider.find_by(name: 'Joyent')
     assert_not_nil provider
     assert_not_empty provider.resources
+
     # As of 2016-03-03, there are 18 compute instance types
     # see https://www.joyent.com/public-cloud/pricing#hardwarevm
-    assert_equal 18, provider.resources.count
+    assert_equal 18, provider.resources.where(resource_type: 'compute').count
+
+    #As of 2016-03-16, there are 2 storage types
+    # see https://www.joyent.com/object-storage/pricing
+    assert_equal 2, provider.resources.where(resource_type: 'storage').count
   end
 end
