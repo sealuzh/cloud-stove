5d6d3318d31ad7cb131758baeb70ed5a0b9a22fd
Genc Mazlami <genc.mazlami@uzh.ch>
2016-03-16 17:44:45 +0100

    Add storage updater for Joyent.

--- app/provider_updaters/joyent_updater.rb
+++ app/provider_updaters/joyent_updater.rb
@@ -1,25 +1,78 @@
 class JoyentUpdater < ProviderUpdater
   def perform
-    uri = URI('https://www.joyent.com/assets/js/pricing.json')
-    
-    pricelist = JSON.load(open(uri))
-    
-    provider = Provider.find_or_create_by(name: 'Joyent')
-    provider.more_attributes['pricelist'] = pricelist
-    provider.save!
-    
-    pricelist['Portfolio'].each do |instance_type|
-      # For now, we only store VM types, no containers
-      next unless instance_type['OS'] == 'Hardware VM'
-      
-      resource_id = instance_type['API Name']
-      resource = provider.resources.find_or_create_by(name: resource_id)
-      resource.more_attributes['type'] = 'compute'
-      resource.more_attributes['price_per_hour'] = instance_type['Price']
-      resource.more_attributes['cores'] = instance_type['vCPUs']
-      resource.more_attributes['mem_gb'] = instance_type['RAM GiB']
-      resource.more_attributes['bandwidth_gbps'] = instance_type['Network']
-      resource.save!
-    end
+    update_provider
+    update_compute
+    update_storage
   end
+
+
+  private
+
+    def update_provider
+      #TODO:  Add SLA for provider. CAUTION! Joyent has no 'actual' availability SLA, they claim 100%, but in reality it's around 0.9993, if you follow their example.
+      # See here: https://www.joyent.com/about/policies/cloud-hosting-service-level-agreement
+
+      provider = Provider.find_or_create_by(name: 'Joyent')
+      provider.more_attributes['pricelist'] = {}
+      provider.save!
+    end
+
+    def update_compute
+      uri = URI('https://www.joyent.com/assets/js/pricing.json')
+
+      pricelist = JSON.load(open(uri))
+
+      provider = Provider.find_or_create_by(name: 'Joyent')
+      provider.more_attributes['pricelist']['compute'] = pricelist
+      provider.save!
+
+      pricelist['Portfolio'].each do |instance_type|
+        # For now, we only store VM types, no containers
+        next unless instance_type['OS'] == 'Hardware VM'
+
+        resource_id = instance_type['API Name']
+        resource = provider.resources.find_or_create_by(name: resource_id)
+        resource.resource_type = 'compute'
+        resource.more_attributes['price_per_hour'] = instance_type['Price']
+        resource.more_attributes['cores'] = instance_type['vCPUs']
+        resource.more_attributes['mem_gb'] = instance_type['RAM GiB']
+        resource.more_attributes['bandwidth_gbps'] = instance_type['Network']
+        resource.save!
+
+      end
+
+    end
+
+    def update_storage
+      provider = Provider.find_or_create_by(name: 'Joyent')
+
+      uri = URI('https://www.joyent.com/object-storage/pricing')
+      doc = Nokogiri::HTML(open(uri))
+
+      storage_div = doc.css('div#storage')
+
+      pricelist = {}
+
+      storage_div.css('table').css('thead').css('tr').css('th').each_with_index do |resource_name, index|
+        next if (index==0)
+
+        puts "RESOURCE_NAME: #{resource_name.text}"
+
+
+        resource = provider.resources.find_or_create_by(name: resource_name.text)
+        resource.resource_type = 'storage'
+        resource.more_attributes['price_per_month_gb'] = storage_div.css('table').css('tbody').css('tr')[0].css('td')[index].text.delete('^0-9.').to_d
+        resource.save!
+
+        pricelist[resource.name] = {
+            'type' => 'storage',
+            'price_per_month_gb' => resource.more_attributes['price_per_month_gb']
+        }
+      end
+
+      provider.more_attributes['pricelist']['storage'] = pricelist
+      provider.save!
+
+
+    end
 end
