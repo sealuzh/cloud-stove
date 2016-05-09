e84aa6777e9c15bd19898c8280920fd74c944c76
Genc Mazlami <genc.mazlami@uzh.ch>
2016-05-09 14:36:25 +0200

    Bidirectional self-references on ingredient for nesting.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,12 @@
 class Ingredient < Base
+  #each ingredient can have a template and has a parent that allows nesting composite ingredients
   belongs_to :parent, class_name: "Ingredient"
   belongs_to :template, class_name: "Ingredient"
+
+  #the reverse relations to the ones above
+  has_many :children, class_name: "Ingredient", foreign_key: "parent_id"
+  has_one :instance, class_name: "Ingredient", foreign_key: "template_id"
+
   has_many :constraints
+
 end
