b1436bc36abb1d58cd1f3663d301646874542056
Genc Mazlami <genc.mazlami@uzh.ch>
2016-06-03 16:02:00 +0200

    Add before_save method to attach source ingredient to ingredient_id of constraint.

--- app/models/dependency_constraint.rb
+++ app/models/dependency_constraint.rb
@@ -1,4 +1,12 @@
 class DependencyConstraint < Constraint
+  belongs_to :ingredient
   belongs_to :source, class_name: 'Ingredient'
   belongs_to :target, class_name: 'Ingredient'
+  before_save :attach_source_ingredient
+
+
+  private
+    def attach_source_ingredient
+      self.ingredient = self.source
+    end
 end
