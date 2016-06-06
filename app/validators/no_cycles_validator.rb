950c7da556f2c3b18e0499ab5f94e21318f3661a
Genc Mazlami <genc.mazlami@uzh.ch>
2016-06-06 18:05:40 +0200

    Fix NoCyclesValidator semantics

--- app/validators/no_cycles_validator.rb
+++ app/validators/no_cycles_validator.rb
@@ -2,7 +2,7 @@ class NoCyclesValidator < ActiveModel::Validator
   def validate(record)
     id = record.id
     current = record
-    while ! current.parent.nil? do
+    while ! current.parent.nil? && !current.id.nil? && !id.nil? do
       if current.parent.id == id
         record.errors[:nocycle] << 'Creating cyclic ingredient hierarchies is not allowed!'
         break
