7737dcfef35e8569126416da095dd4e9d9327f5b
Genc Mazlami <genc.mazlami@uzh.ch>
2016-06-07 10:39:35 +0200

    Drop legacy data model tables.

--- db/schema.rb
+++ db/schema.rb
@@ -11,59 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519131446) do
-
-  create_table "application_deployment_recommendations", force: :cascade do |t|
-    t.text     "more_attributes",      default: "{}", null: false
-    t.integer  "cloud_application_id"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.decimal  "total_cost"
-    t.integer  "provider_id"
-    t.string   "provider_name"
-  end
-
-  add_index "application_deployment_recommendations", ["cloud_application_id"], name: "index_app_dep_rec_on_cloud_app_id"
-
-  create_table "blueprints", force: :cascade do |t|
-    t.string   "name"
-    t.text     "more_attributes", default: "{}", null: false
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-  end
-
-  create_table "cloud_applications", force: :cascade do |t|
-    t.string   "name"
-    t.text     "more_attributes", default: "{}", null: false
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.integer  "blueprint_id"
-  end
-
-  add_index "cloud_applications", ["blueprint_id"], name: "index_cloud_applications_on_blueprint_id"
-
-  create_table "components", force: :cascade do |t|
-    t.string   "name"
-    t.string   "component_type"
-    t.text     "more_attributes", default: "{}", null: false
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.integer  "blueprint_id"
-  end
-
-  add_index "components", ["blueprint_id"], name: "index_components_on_blueprint_id"
-
-  create_table "concrete_components", force: :cascade do |t|
-    t.string   "name"
-    t.text     "more_attributes",      default: "{}", null: false
-    t.integer  "component_id"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.integer  "cloud_application_id"
-  end
-
-  add_index "concrete_components", ["cloud_application_id"], name: "index_concrete_components_on_cloud_application_id"
-  add_index "concrete_components", ["component_id"], name: "index_concrete_components_on_component_id"
+ActiveRecord::Schema.define(version: 20160607083407) do
 
   create_table "constraints", force: :cascade do |t|
     t.integer  "ingredient_id"
@@ -94,28 +42,6 @@ ActiveRecord::Schema.define(version: 20160519131446) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
-
-  create_table "deployment_recommendations", force: :cascade do |t|
-    t.text     "more_attributes"
-    t.integer  "slo_set_id"
-    t.datetime "created_at",                               null: false
-    t.datetime "updated_at",                               null: false
-    t.decimal  "total_cost"
-    t.string   "provider"
-    t.integer  "provider_id"
-    t.integer  "application_deployment_recommendation_id"
-  end
-
-  add_index "deployment_recommendations", ["slo_set_id"], name: "index_deployment_recommendations_on_slo_set_id"
-
-  create_table "deployment_rules", force: :cascade do |t|
-    t.text     "more_attributes"
-    t.integer  "component_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "deployment_rules", ["component_id"], name: "index_deployment_rules_on_component_id"
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -149,14 +75,5 @@ ActiveRecord::Schema.define(version: 20160519131446) do
   end
 
   add_index "resources", ["provider_id"], name: "index_resources_on_provider_id"
-
-  create_table "slo_sets", force: :cascade do |t|
-    t.text     "more_attributes",       default: "{}", null: false
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
-    t.integer  "concrete_component_id"
-  end
-
-  add_index "slo_sets", ["concrete_component_id"], name: "index_slo_sets_on_concrete_component_id"
 
 end
