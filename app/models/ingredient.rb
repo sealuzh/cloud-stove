bf04b6c2fb0cf1da534445367311156a81bcedcb
Genc Mazlami <genc.mazlami@uzh.ch>
2016-06-08 16:50:18 +0200

    Copying a subtree of an ingredient hierarchy is now also possible.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -45,18 +45,40 @@ class Ingredient < Base
   end
 
   def copy
+    # copiesh_hash: hash that maps ingredient ids (keys) of the original ingredients to the newly created copies (values)
+    # root_copy: the root ingredient of the new (copied) hierarchy
     copies_hash, root_copy = deep_dup({},self)
 
+    # get all dependency constraints of the original root ingredient, to copy them onto the new structure
     dependency_constraints = all_dependency_constraints
 
-    dependency_constraints.each do |dependency_constraint|
-      d = DependencyConstraint.new
-      d.source = copies_hash[dependency_constraint.source.id]
-      d.ingredient = copies_hash[dependency_constraint.source.id]
-      d.target = copies_hash[dependency_constraint.target.id]
-      d.save!
+    if self.parent
+      # attach to parent of origin if there is any
+      root_copy.parent = self.parent
+
+      # if only a subtree is copied (e.g. the root_copy has a parent), there may be dependency constraints to ingredients that did not get copied
+      # because they were outside the subtree being copied (e.g. they have no entry in the copies_hash)
+      # then we use the original ingredient as source/target instead of the non-existing copy
+      dependency_constraints.each do |dependency_constraint|
+        d = DependencyConstraint.new
+        d.source = (copies_hash[dependency_constraint.source.id]) ? copies_hash[dependency_constraint.source.id] : Ingredient.find(dependency_constraint.source.id)
+        d.ingredient = (copies_hash[dependency_constraint.source.id]) ? copies_hash[dependency_constraint.source.id] : Ingredient.find(dependency_constraint.source.id)
+        d.target = (copies_hash[dependency_constraint.target.id]) ? copies_hash[dependency_constraint.target.id] : Ingredient.find(dependency_constraint.target.id)
+        d.save!
+      end
+
+    else
+      dependency_constraints.each do |dependency_constraint|
+        d = DependencyConstraint.new
+        d.source = copies_hash[dependency_constraint.source.id]
+        d.ingredient = copies_hash[dependency_constraint.source.id]
+        d.target = copies_hash[dependency_constraint.target.id]
+        d.save!
+      end
     end
-    return root_copy
+
+    root_copy.save!
+    root_copy
   end
 
   private
@@ -80,7 +102,7 @@ class Ingredient < Base
     return copies_hash,copy
   end
 
-  # recursive postorder tree traversal method that returns a hash with all constraints found
+  # recursive postorder tree traversal method that returns a hash with all dependency constraints found in the subtree
   def dependency_constraints(current_ingredient,constraint_hash)
       current_ingredient.children.all.each do |child|
         constraint_hash.merge(dependency_constraints(child,constraint_hash))
