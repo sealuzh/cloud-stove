1e1d272a7ff87ab8a0fc1c04d8c5a3cbd02ab01a
Genc Mazlami <genc.mazlami@uzh.ch>
2016-06-08 19:43:06 +0200

    Fix Validator for DependencyConstraint.

--- app/validators/no_such_ingredient_validator.rb
+++ app/validators/no_such_ingredient_validator.rb
@@ -6,8 +6,5 @@ class NoSuchIngredientValidator < ActiveModel::Validator
     if !Ingredient.exists?(record.source_id)
       record.errors[:no_such_source] = 'The specified source ingredient does not exist!'
     end
-    if !Ingredient.exists?(record.ingredient_id)
-      record.errors[:no_such_ingredient] = 'The specified ingredient does not exist!'
-    end
   end
 end
