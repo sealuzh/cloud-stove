5416fcd765dc2617167b91de46f89e5a415ba184
Joel Scheuner <joel.scheuner.dev@gmail.com>
2016-06-13 20:46:42 +0200

    Fix too loose inclusive origin match
    Notice that regex matches are inclusive by default according to:
    https://github.com/cyu/rack-cors#middleware-options
    "It is recommended that any regular expression be enclosed with start & end string anchors (\A\z)."

--- config/application.rb
+++ config/application.rb
@@ -32,8 +32,7 @@ module CloudStove
     # CORS Configuration for handling preflight RequestService
     config.middleware.insert_before 0, 'Rack::Cors' do
       allow do
-        origins 'localhost:1232', '127.0.0.1:1232', /serene-garden-85460-pr-\d+.herokuapp.com/, 'serene-garden-85460.herokuapp.com' , 'staging.frontend.thestove.io'
-
+        origins 'localhost:1232', '127.0.0.1:1232', /\Ahttps:\/\/serene-garden-85460-pr-\d+\.herokuapp\.com.*\z/, 'serene-garden-85460.herokuapp.com' , 'staging.frontend.thestove.io'
         resource '/cors',
                  :headers => :any,
                  :methods => [:post],
