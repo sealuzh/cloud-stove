502033f974191ef42df8fd5bb65b356b1de1ddaa
Joel Scheuner <joel.scheuner.dev@gmail.com>
2016-06-13 19:12:34 +0200

    Enable cors config

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# require 'rack'
-# require 'rack/cors'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -37,23 +34,22 @@ module CloudStove
     # Access using `Rails.configuration.x`
     config.x.gravatar_host = 'www.gravatar.com'
 
-    # # CORS Configuration for handling preflight RequestService
-    # config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
-    #       allow do
-    #         origins 'localhost:1232', '127.0.0.1:1232', 'staging.frontend.thestove.io'
-    #
-    #         resource '/cors',
-    #           :headers => :any,
-    #           :methods => [:post],
-    #           :credentials => true,
-    #           :max_age => 0
-    #
-    #         resource '*',
-    #           :headers => :any,
-    #           :methods => [:get, :post, :delete, :put, :patch, :options, :head],
-    #           :max_age => 0
-    #       end
-    # end
+    # CORS Configuration for handling preflight RequestService
+    config.middleware.insert_before 0, 'Rack::Cors', :debug => true, :logger => (-> { Rails.logger }) do
+          allow do
+            origins 'localhost:1232', '127.0.0.1:1232', 'staging.frontend.thestove.io'
 
+            resource '/cors',
+              :headers => :any,
+              :methods => [:post],
+              :credentials => true,
+              :max_age => 0
+
+            resource '*',
+              :headers => :any,
+              :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+              :max_age => 0
+          end
+    end
   end
 end
