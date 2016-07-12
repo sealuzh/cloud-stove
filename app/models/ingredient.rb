710b3f6defacd697d885f0d2922ed4868ad79de6
Joel Scheuner <joel.scheuner.dev@gmail.com>
2016-07-12 17:15:08 +0200

    Improve readability of Ingredients class

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -92,7 +92,6 @@ class Ingredient < Base
     engine.instantiate(self)
   end
 
-
   def schedule_recommendation_job
     preferred_providers = self.provider_constraint.provider_list rescue [nil]
     jobs = []
@@ -106,51 +105,48 @@ class Ingredient < Base
 
   # looks for the root of the application hierarchy of this ingredient and returns it
   def application_root
-      if self.parent.nil?
-        return self
-      else
-        return self.parent.application_root
-      end
+    if self.parent.nil?
+      self
+    else
+      self.parent.application_root
+    end
   end
 
   private
 
+    def deep_dup(copies_hash,current)
+      copy = current.dup
+      copy.cpu_constraint = current.cpu_constraint.dup if current.cpu_constraint.present?
+      copy.ram_constraint = current.ram_constraint.dup if current.ram_constraint.present?
+      copies_hash[current.id] = copy
 
-  def deep_dup(copies_hash,current)
-    copy = current.dup
-    copy.cpu_constraint = current.cpu_constraint.dup unless current.cpu_constraint.nil?
-    copy.ram_constraint = current.ram_constraint.dup unless current.ram_constraint.nil?
-    copies_hash[current.id] = copy
+      current.children.each do |child|
+        copies_hash.merge(deep_dup(copies_hash,child)[0])
+      end
 
-    current.children.each do |child|
-      copies_hash.merge(deep_dup(copies_hash,child)[0])
+      if !copies_hash.empty? && !current.parent.nil?
+        if copies_hash[current.parent.id]
+          copy.parent = copies_hash[current.parent.id]
+        end
+      end
+
+      copy.save!
+
+      return copies_hash,copy
     end
 
-    if !copies_hash.empty? && !current.parent.nil?
-      if copies_hash[current.parent.id]
-        copy.parent = copies_hash[current.parent.id]
-      end
+    # recursive postorder tree traversal method that returns a hash with all dependency constraints found in the subtree
+    def dependency_constraints_rec(current_ingredient, constraint_hash)
+        current_ingredient.children.all.each do |child|
+          constraint_hash.merge(dependency_constraints_rec(child, constraint_hash))
+        end
+
+        current_ingredient.constraints_as_source.all.each do |constraint|
+          constraint_hash[constraint.id] = constraint
+        end
+        current_ingredient.constraints_as_target.all.each do |constraint|
+          constraint_hash[constraint.id] = constraint
+        end
+        constraint_hash
     end
-
-    copy.save!
-
-    return copies_hash,copy
-  end
-
-  # recursive postorder tree traversal method that returns a hash with all dependency constraints found in the subtree
-  def dependency_constraints_rec(current_ingredient, constraint_hash)
-      current_ingredient.children.all.each do |child|
-        constraint_hash.merge(dependency_constraints_rec(child, constraint_hash))
-      end
-
-      current_ingredient.constraints_as_source.all.each do |constraint|
-        constraint_hash[constraint.id] = constraint
-      end
-      current_ingredient.constraints_as_target.all.each do |constraint|
-        constraint_hash[constraint.id] = constraint
-      end
-
-      constraint_hash
-  end
-
 end
