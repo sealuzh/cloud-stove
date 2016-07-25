76eb3a39ef9204409e7ae2888ce19bef3e62f494
Joel Scheuner <joel.scheuner.dev@gmail.com>
2016-07-25 13:56:13 +0200

    Load instead of require seeds in tests
    Require only loads them once thus each seed would be only
    usable once in a test.

--- test/models/deployment_recommendation_test.rb
+++ test/models/deployment_recommendation_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class DeploymentRecommendationTest < ActiveSupport::TestCase
   SEEDS_ROOT = Rails.root + 'db/seeds/'
-  def require_seed(name)
-    require (SEEDS_ROOT + name)
+  def load_seed(name)
+    load ("#{SEEDS_ROOT}#{name}.rb")
   end
 
   test 'generate deployment recommendation' do
     Ingredient.find_or_create_by(name: 'Multitier Architecture')
-    require_seed 'ingredient_instance_rails_app'
+    load_seed 'ingredient_instance_rails_app'
     rails_app = Ingredient.find_by_name('Rails Application with PostgreSQL Backend')
     create(:amazon_provider)
     create(:google_provider)
