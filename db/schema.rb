bc3699ed0e21c848af7368b29fd6a76290e60e7f
Genc Mazlami <genc.mazlami@uzh.ch>
2016-08-30 15:30:00 +0200

    Replace slope with parallelism.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830124345) do
+ActiveRecord::Schema.define(version: 20160830132649) do
 
   create_table "constraints", force: :cascade do |t|
     t.integer  "ingredient_id"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20160830124345) do
 
   create_table "cpu_workloads", force: :cascade do |t|
     t.integer  "cspu_user_capacity"
-    t.float    "cspu_slope"
+    t.float    "parallelism"
     t.integer  "ingredient_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
