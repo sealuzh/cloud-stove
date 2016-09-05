c130809c414cf0cc989e0d2d8789807282c91d4b
Joel Scheuner <joel.scheuner.dev@gmail.com>
2016-09-06 01:27:41 +0200

    Set JSON as default for API namespace

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # https://github.com/lynndylanhurley/devise_token_auth#can-i-use-this-gem-alongside-standard-devise
   devise_for :users
   # Token auth routes available at `/api/auth`
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     mount_devise_token_auth_for 'User', at: 'auth'
   end
 
