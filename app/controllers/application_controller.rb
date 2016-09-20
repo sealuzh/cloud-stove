f9c9b0d66d932bb72d630e4b4e07ccfe137a4cac
Joel Scheuner <joel.scheuner.dev@gmail.com>
2016-09-20 20:56:44 +0200

    Redirect non-authenticated users to the login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  include IngredientsHelper
+  before_filter :require_login, unless: :devise_controller?
+
+  private
+
+    def require_login
+      redirect_to new_user_session_url unless user_signed_in?
+    end
 end
