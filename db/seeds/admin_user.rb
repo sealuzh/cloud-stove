c672464e0a428af6fbd59bf26a64f1a1cf364610
Joel Scheuner <joel.scheuner.dev@gmail.com>
2016-09-21 16:55:24 +0200

    Shorten default password for development
    The frontend currently requires at least 8 characters
    => `admin` would be too short

--- db/seeds/admin_user.rb
+++ db/seeds/admin_user.rb
@@ -1,7 +1,8 @@
 # Add an initial admin user
+admin_password = 'stove-admin'
 User.create!({
   email: 'admin@thestove.io',
-  password: 'seal-stove-admin',
-  password_confirmation: 'seal-stove-admin',
+  password: admin_password,
+  password_confirmation: admin_password,
   is_admin: true
 })
