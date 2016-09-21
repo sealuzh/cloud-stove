b77d97499f5b9c191d63b697617c918bc681be8e
Joel Scheuner <joel.scheuner.dev@gmail.com>
2016-09-21 19:05:58 +0200

    Isolate tests from seeds
    Using separate seeds based on constraints for test.
    Test seeds have the `_test` suffix
    
    * Use a setup block to reduce duplication in the deployment recommendation tests

---
+++ db/seeds/ingredient_instance_rails_app_test.rb
@@ -0,0 +1,99 @@
+begin
+rails_app_template =  Ingredient.create!(name: 'Rails Application with PostgreSQL Backend', is_template: true)
+rails_app_instance = Ingredient.create!(
+  template_id: rails_app_template.id,
+  name: 'Rails Application with PostgreSQL Backend',
+  body: <<HERE
+A traditional wep application, let's say a web shop with
+
+* Rails as the application server
+  * Devise for authentication of authors and editors
+  * Pundit for authorization
+* PostgreSQL as database
+  * Products are indexed on
+    * name
+    * categories
+HERE
+)
+rails_app_instance.user_workload = UserWorkload.create!(
+  num_simultaneous_users: 200
+)
+rails_app_instance.provider_constraint = ProviderConstraint.create!(
+  preferred_providers: 'Amazon'
+)
+rails_app_instance.preferred_region_area_constraint = PreferredRegionAreaConstraint.create!(
+  preferred_region_area: 'EU'
+)
+
+db = rails_app_instance.children.create!(
+  name: 'PostgreSQL',
+  body: <<HERE
+Specific things about the PostgreSQL db.
+HERE
+)
+db.ram_workload = RamWorkload.create!(
+  ram_mb_required: 1500,
+  ram_mb_required_user_capacity: 200,
+  ram_mb_growth_per_user: 0.007)
+db.cpu_workload = CpuWorkload.create!(
+  cspu_user_capacity: 1500,
+  parallelism: 0.9
+)
+db.constraints << RamConstraint.create(
+    min_ram: 2048
+)
+db.constraints << CpuConstraint.create(
+    min_cpus: 1
+)
+
+app = rails_app_instance.children.create!(
+  name: 'Rails App',
+  body: <<HERE
+Specific things about the Rails app.
+HERE
+)
+app.ram_workload = RamWorkload.create!(
+  ram_mb_required: 450,
+  ram_mb_required_user_capacity: 100,
+  ram_mb_growth_per_user: 1)
+app.cpu_workload = CpuWorkload.create!(
+  cspu_user_capacity: 500,
+  parallelism: 0.97
+)
+app.constraints << DependencyConstraint.create!(
+  source: app,
+  target: db
+)
+app.constraints << RamConstraint.create(
+    min_ram: 4096
+)
+app.constraints << CpuConstraint.create(
+    min_cpus: 1
+)
+
+lb = rails_app_instance.children.create!(
+    name: 'NGINX',
+    body: <<HERE
+Specific things about the NGINX load balancer.
+HERE
+)
+lb.ram_workload = RamWorkload.create!(
+  ram_mb_required: 2000,
+  ram_mb_required_user_capacity: 2400,
+  ram_mb_growth_per_user: 0.008)
+lb.cpu_workload = CpuWorkload.create!(
+  cspu_user_capacity: 3500,
+  parallelism: 0.8
+)
+lb.constraints << DependencyConstraint.create!(
+  source: lb,
+  target: app
+)
+lb.constraints << RamConstraint.create(
+    min_ram: 1024
+)
+lb.constraints << CpuConstraint.create(
+    min_cpus: 1
+)
+rails_app_instance.assign_user!(User.admin.first)
+end
--- test/models/deployment_recommendation_test.rb
+++ test/models/deployment_recommendation_test.rb
@@ -1,24 +1,27 @@
 require 'test_helper'
 
+# NOTICE: Workloads are not considered here unless
+# `@rails_app.update_constraints` is called explicitly
 class DeploymentRecommendationTest < ActiveSupport::TestCase
   SEEDS_ROOT = Rails.root + 'db/seeds/'
   def load_seed(name)
     load ("#{SEEDS_ROOT}#{name}.rb")
   end
 
+  setup do
+    load_seed 'admin_user'
+    load_seed 'ingredient_instance_rails_app_test'
+    @rails_app = Ingredient.where(name: 'Rails Application with PostgreSQL Backend', is_template: false).first
+  end
+
   test 'generate deployment recommendation' do
-    Ingredient.find_or_create_by(name: 'Multitier Architecture')
-    load_seed 'ingredient_instance_rails_app'
-    rails_app = Ingredient.find_by_name('Rails Application with PostgreSQL Backend')
-    build(:user_workload, ingredient: rails_app)
-    rails_app.user_workload = UserWorkload.new(ingredient: rails_app, num_simultaneous_users: 200)
     create(:amazon_provider)
     create(:google_provider)
 
-    recommendation = DeploymentRecommendation.construct(rails_app)
+    recommendation = DeploymentRecommendation.construct(@rails_app)
 
     expected_resources = %w(c3.2xlarge c3.2xlarge t2.micro).collect { |n|  Resource.find_by_name(n) }
-    ingredient_ids = rails_app.children.sort_by(&:id).map(&:id)
+    ingredient_ids = @rails_app.children.sort_by(&:id).map(&:id)
     resource_ids = expected_resources.collect(&:id)
     ingredients_hash = Hash[ingredient_ids.zip(resource_ids)]
     region_codes = expected_resources.collect(&:region_code)
@@ -37,19 +40,15 @@ class DeploymentRecommendationTest < ActiveSupport::TestCase
   end
 
   test 'region constraint on root ingredient' do
-    Ingredient.find_or_create_by(name: 'Multitier Architecture')
-    load_seed 'ingredient_instance_rails_app'
-    rails_app = Ingredient.find_by_name('Rails Application with PostgreSQL Backend')
-    build(:user_workload, ingredient: rails_app)
-    rails_app.preferred_region_area_constraint = PreferredRegionAreaConstraint.create(preferred_region_area: 'US')
+    @rails_app.preferred_region_area_constraint = PreferredRegionAreaConstraint.create(preferred_region_area: 'US')
     create(:amazon_provider)
     create(:google_provider)
     create(:azure_provider)
 
-    recommendation = DeploymentRecommendation.construct(rails_app)
+    recommendation = DeploymentRecommendation.construct(@rails_app)
 
     expected_resources = %w(A2 A3 A1).collect { |n|  Resource.find_by_name(n) }
-    ingredient_ids = rails_app.children.sort_by(&:id).map(&:id)
+    ingredient_ids = @rails_app.children.sort_by(&:id).map(&:id)
     resource_ids = expected_resources.collect(&:id)
     ingredients_hash = Hash[ingredient_ids.zip(resource_ids)]
     region_codes = expected_resources.collect(&:region_code)
@@ -63,21 +62,17 @@ class DeploymentRecommendationTest < ActiveSupport::TestCase
   end
 
   test 'hierarchical region constraint' do
-    Ingredient.find_or_create_by(name: 'Multitier Architecture')
-    load_seed 'ingredient_instance_rails_app'
-    rails_app = Ingredient.find_by_name('Rails Application with PostgreSQL Backend')
-    build(:user_workload, ingredient: rails_app)
-    rails_app.preferred_region_area_constraint = PreferredRegionAreaConstraint.create(preferred_region_area: 'US')
+    @rails_app.preferred_region_area_constraint = PreferredRegionAreaConstraint.create(preferred_region_area: 'US')
     lb = Ingredient.find_by_name('NGINX')
     lb.preferred_region_area_constraint = PreferredRegionAreaConstraint.create(preferred_region_area: 'EU')
     create(:amazon_provider)
     create(:google_provider)
     create(:azure_provider)
 
-    recommendation = DeploymentRecommendation.construct(rails_app)
+    recommendation = DeploymentRecommendation.construct(@rails_app)
 
     expected_resources = %w(A2 A3 t2.micro).collect { |n|  Resource.find_by_name(n) }
-    ingredient_ids = rails_app.children.sort_by(&:id).map(&:id)
+    ingredient_ids = @rails_app.children.sort_by(&:id).map(&:id)
     resource_ids = expected_resources.collect(&:id)
     ingredients_hash = Hash[ingredient_ids.zip(resource_ids)]
     region_codes = expected_resources.collect(&:region_code)
@@ -91,14 +86,13 @@ class DeploymentRecommendationTest < ActiveSupport::TestCase
   end
 
   test 'unsatisfiable constraint' do
-    Ingredient.find_or_create_by(name: 'Multitier Architecture')
-    load_seed 'ingredient_instance_rails_app'
-    rails_app = Ingredient.find_by_name('Rails Application with PostgreSQL Backend')
-    build(:user_workload, ingredient: rails_app)
-    create(:google_provider)
+    create(:amazon_provider)
+    rc = @rails_app.children.first.ram_constraint
+    rc.min_ram = 16_000_000
+    rc.save!
 
-    recommendation = DeploymentRecommendation.construct(rails_app)
-    # Google provider factories have no instance available to satisfy the 4G RAM constraint
+    recommendation = DeploymentRecommendation.construct(@rails_app)
+    # No instance available with more than 15GB RAM
     assert_equal 'unsatisfiable', recommendation.status
     assert_equal 'constraint forall(i in Ingredients)(ram[assignments[i]] >= min_ram[i]);', recommendation.more_attributes['unsatisfiable_message']
   end
