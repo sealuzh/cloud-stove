7e0063e771d2dc9f3d2fde58f4f48314e8b4fd3f
Joel Scheuner <joel.scheuner.dev@gmail.com>
2016-09-24 00:00:14 +0200

    Use Azure APIs to query VM size data
    * No pricing data included in API responses !
    * Region area is not detected yet
    
    Options:
    a) Write a new crawler for the updated pricing Web page
    b) Combine the data from API somehow with the pricing Web page
    c) Query a 3rd party service that maintains their own crawlers or manually updates prices

--- app/provider_updaters/azure_updater.rb
+++ app/provider_updaters/azure_updater.rb
@@ -1,93 +1,74 @@
+require 'azure_mgmt_resources'
+require 'azure_mgmt_compute'
+
 class AzureUpdater < ProviderUpdater
   def perform
-    update_provider
-    update_compute
-    update_storage
+    azure = Provider.find_or_create_by(name: 'Microsoft Azure')
+    create_compute_resources(azure)
+    azure.save!
+  end
+
+  def create_compute_resources(provider)
+    regions.each do |region|
+      vm_sizes(region).each do |vm|
+        create_compute_resource(provider, region, vm)
+      end
+    end
+  end
+
+  def create_compute_resource(provider, region, vm)
+    resource = provider.resources.find_or_create_by(name: vm.name, region: region)
+    resource.resource_type = 'compute'
+    resource.region_area ='EU' # TODO: extract_region_area(region)
+    resource.more_attributes = more_attributes(vm)
+    resource.save!
+  end
+
+  def regions
+    resource_client = Azure::ARM::Resources::ResourceManagementClient.new(credentials(app_token))
+    resource_client.subscription_id = subscription_id
+    providers_api = Azure::ARM::Resources::Providers.new(resource_client)
+    provider_response = providers_api.get('Microsoft.Compute')
+    provider_response.resource_types.detect { |resource_type| resource_type.resource_type == 'locations/vmSizes' }.locations
+  end
+
+  # Returns `Array<VirtualMachineSize>`
+  # VirtualMachineSize: http://www.rubydoc.info/gems/azure_mgmt_compute/0.6.0/Azure/ARM/Compute/Models/VirtualMachineSize
+  def vm_sizes(region)
+    compute_client = Azure::ARM::Compute::ComputeManagementClient.new(credentials(app_token))
+    compute_client.subscription_id = subscription_id
+    vm_sizes_api = Azure::ARM::Compute::VirtualMachineSizes.new(compute_client)
+    vm_sizes_api.list(region).value
+  end
+
+  def more_attributes(vm)
+    {
+        'cores' => vm.number_of_cores,
+        'mem_gb' => vm.memory_in_mb.to_f / 1000,
+        'price_per_hour' => '0.0065' # TODO: extract price !!!
+    }
   end
 
   private
 
-    def update_provider
-      provider = Provider.find_or_create_by(name: 'Microsoft Azure')
-      provider.more_attributes['pricelist'] = {}
-      provider.more_attributes['sla'] = {}
-      provider.more_attributes['sla']['storage'] = extract_sla('https://azure.microsoft.com/en-us/support/legal/sla/storage/v1_0/')
-      provider.more_attributes['sla']['compute'] = extract_sla('https://azure.microsoft.com/en-us/support/legal/sla/virtual-machines/v1_0/')
-      provider.save!
+    def subscription_id
+      subscription_id = ENV['AZURE_SUBSCRIPTION_ID']
+      if subscription_id.nil?
+        fail ArgumentError, "#{self.class.to_s} requires `AZURE_SUBSCRIPTION_ID` to be set"
+      end
+      subscription_id
     end
 
-
-    def update_compute
-      uri = URI('https://azure.microsoft.com/en-us/pricing/details/virtual-machines/')
-      doc = Nokogiri::HTML(open(uri))
-      pricelist = {}
-      pricelist_pos = doc.css('[data-tab-panel="tab-panel-os"]').css('[data-href="#Linux"]').attribute('data-id').text
-      pricelist_div = doc.css('[data-tab-panel-id="tab-panel-os"]').css("div:eq(#{pricelist_pos})")
-      pricelist_entries = pricelist_div.css("table > tbody > tr")
-      pricelist_entries.each do |tr|
-        logger.info tr.to_s
-        cells = tr.css('td')
-        # Pricing table rows have 5 cells:
-        # instance id, cores, ram, disk sizes, price
-        next if cells.count < 5
-        resource_id = cells.first.text.gsub(/\s/, '')
-        price_data_span = cells[4].css('>span.price-data')
-        next if price_data_span.empty?
-        data_amount_value = price_data_span.attribute('data-amount')
-        amounts = JSON.load(data_amount_value.to_s)
-        next unless amounts.is_a?(Hash)
-        price_per_hour = amounts["default"]
-        pricelist[resource_id] = {
-            'type' => 'compute',
-            'cores' => cells[1].css('strong').text,
-            'mem_gb' => cells[2].css('strong').text,
-            'price_per_hour' => price_per_hour,
-            'regions' => amounts['regional'],
-        }
-      end
-      provider = Provider.find_or_create_by(name: 'Microsoft Azure')
-      provider.more_attributes['pricelist']['compute'] = pricelist
-      provider.save!
-      pricelist.each_pair do |resource_id, data|
-        data['regions'].each do |region, price|
-          resource = provider.resources.find_or_create_by(name: resource_id, region: region)
-          resource.resource_type = 'compute'
-          resource.more_attributes = data.except('type')
-          resource.more_attributes['price_per_hour'] = price
-          resource.region_area = extract_region_area(region)
-          resource.save!
-        end
-      end
+    def app_token
+      MsRestAzure::ApplicationTokenProvider.new(
+          ENV['AZURE_TENANT_ID'],
+          ENV['AZURE_CLIENT_ID'],
+          ENV['AZURE_CLIENT_SECRET']
+      )
     end
 
-
-    def update_storage
-      uri = URI('https://azure.microsoft.com/en-us/pricing/details/storage/')
-      doc = Nokogiri::HTML(open(uri))
-      pricelist = {}
-      provider = Provider.find_or_create_by(name: 'Microsoft Azure')
-      blob_storage_div = doc.css('div.wa-content.wa-tabs-container.wa-conditionalDisplay')[0]
-      first_tb_prices = blob_storage_div.css('tbody').css('tr').css('td')
-      blob_storage_div.css('thead').css('th').each_with_index do |resource_name,index |
-        next if resource_name.text == ''
-        #skip the first tablehead since that is the description column
-        next unless (index != 0) && (!first_tb_prices[index].children.first.attributes['data-amount'].nil?)
-
-        region_hash = JSON.parse(first_tb_prices[index].children.first.attributes['data-amount'].value)
-        region_hash['regional'].each do |region,price|
-          resource = provider.resources.find_or_create_by(name: resource_name.text, region: region)
-          resource.resource_type = 'storage'
-          resource.region_area = extract_region_area(region)
-          resource.more_attributes['price_per_month_gb'] = price
-          resource.save!
-          pricelist[resource_name.text] = {
-              'type' => 'storage',
-              'price_per_month_gb' => resource.more_attributes['price_per_month_gb']
-          }
-        end
-      end
-      provider.more_attributes['pricelist']['storage'] = pricelist
-      provider.save!
+    def credentials(provider)
+      MsRest::TokenCredentials.new(provider)
     end
 
     def extract_region_area(region)
