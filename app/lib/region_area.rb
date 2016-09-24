4d108d1abec3ea3d74c9f5019d4238b111d78f6e
Joel Scheuner <joel.scheuner.dev@gmail.com>
2016-09-24 17:48:18 +0200

    Add region area module
    Provides a helper method that generalizes the heuristic
    to identify region areas.

---
+++ app/lib/region_area.rb
@@ -0,0 +1,18 @@
+# Define the region area mappings in a constant called
+# `RegionArea::PREFIXES` as for example:
+# include RegionArea
+# RegionArea::PREFIXES = {
+#     'us' => 'US',
+#     'eu' => 'EU',
+# }
+module RegionArea
+  UNKNOWN = 'UNKNOWN'
+  def extract_region_area(region)
+    PREFIXES.each do |prefix, region_area|
+      return region_area if region.start_with?(prefix)
+    end
+    puts "WARNING: Could not match region `#{region}` to a region area.
+                     Check `REGION_AREA_PREFIXES` in `AzureUpdater`!"
+    UNKNOWN
+  end
+end
