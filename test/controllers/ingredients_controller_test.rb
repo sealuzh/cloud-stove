e6ca33f310cb6635b7f73487164629268ba4fa37
Joel Scheuner <joel.scheuner.dev@gmail.com>
2016-10-11 12:01:20 +0200

    Add basic auth tests for ingredient controller

--- test/controllers/ingredients_controller_test.rb
+++ test/controllers/ingredients_controller_test.rb
@@ -10,4 +10,29 @@ class IngredientsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+
+  ### Authentication
+  test 'open access to listing templates' do
+    api_non_auth_header
+    get :templates
+    assert_response :success
+  end
+
+  test 'non-authenticated user cannot list applications' do
+    api_non_auth_header
+    get :index
+    assert_response :unauthorized
+  end
+
+  test 'non-admin user cannot create an ingredient' do
+    get :template, ingredient_id: create(:ingredient).id
+    assert_response :forbidden
+  end
+
+  test 'admin user can create an ingredient' do
+    admin = create(:user, :admin)
+    api_auth_header(admin)
+    get :template, ingredient_id: create(:ingredient, user: admin).id
+    assert_response :success
+  end
 end
