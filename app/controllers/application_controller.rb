1bf8056d308645d21d33bb542b24a6d267c7adb7
Joel Scheuner <joel.scheuner.dev@gmail.com>
2016-10-11 13:12:50 +0200

    Provide global `authenticate_admin!` helper
    Usage:
    ```
    before_action :authenticate_admin!
    ```
    
    Can be combined with `only: [:create]` or `except: [:index, :show]`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  # Never redirect on for JSON API
+  # Never redirect to sign in page for JSON API
   before_filter :require_login, unless: :json_request?
 
   private
+
+    def authenticate_admin!
+      unless current_user.try(:is_admin?)
+        forbidden_response
+      end
+    end
+
+    def forbidden_response
+      respond_to do |format|
+        format.html { flash[:error] = 'Only admin users are allowed to perform this action.'; redirect_to :back }
+        format.json { render json: { errors: ['Authorized admins only.'] }, status: :forbidden }
+      end
+    end
 
     def require_login
       redirect_to new_user_session_url unless (devise_controller? || user_signed_in?)
