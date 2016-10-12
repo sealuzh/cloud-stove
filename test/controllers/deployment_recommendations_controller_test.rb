9400c182bd7a5ba018bc7ff544e893ac389d7350
Joel Scheuner <joel.scheuner.dev@gmail.com>
2016-10-12 12:01:52 +0200

    Add generate deployment recommendation test

--- test/controllers/deployment_recommendations_controller_test.rb
+++ test/controllers/deployment_recommendations_controller_test.rb
@@ -12,4 +12,17 @@ class DeploymentRecommendationsControllerTest < ActionController::TestCase
     assert_equal rails_app.children[0].name, json_response[0]['recommendation'][0]['ingredient']['name']
     assert_equal 't2.micro', json_response[0]['recommendation'][0]['resource']['name']
   end
+
+  test 'generate recommendations' do
+    rails_app = create(:rails_app, user: @user)
+    rails_app.user_workload = create(:user_workload, user: @user)
+    rails_app.children.each do |child|
+      child.cpu_workload = create(:cpu_workload, user: @user)
+      child.ram_workload = create(:ram_workload, user: @user)
+    end
+
+    put :trigger, ingredient_id: rails_app.id
+    assert_response :ok
+    assert_equal ' ComputeRecommendationJob', JobWrapper.find_by_uuid(json_response['job_id']).job_type
+  end
 end
