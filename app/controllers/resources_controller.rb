c9345e40c1333748078ffdc32c9b8b70521753e8
Dominik Schöni <dominik@dominikschoeni.ch>
2016-11-29 13:20:08 +0100

    making resources accessible for non-admins

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,5 +1,5 @@
 class ResourcesController < ApplicationController
-  before_action :authenticate_admin!
+  #before_action :authenticate_admin!
 
   def index
     @resources = Resource.page(params[:page])
