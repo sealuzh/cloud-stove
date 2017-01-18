4514bf26f684cab34bbc8452597db15663f4f156
Joel Scheuner <joel.scheuner.dev@gmail.com>
2017-01-18 13:48:18 +0100

    Extract job tracking into separate module

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < Base
+  include JobTracking
+
   belongs_to :user
 
   # Each ingredient can have a parent that allows nesting composite ingredients
@@ -172,21 +174,6 @@ class Ingredient < Base
         end
       end
     end
-  end
-
-  def add_job(job_id)
-    # Notice that the `more_attributes` serialization mechanism converts the set into an array
-    ma['jobs'].present? ? ma['jobs'] = Set.new(ma['jobs']).add(job_id) : ma['jobs'] = Set.new([job_id])
-    self.save!
-  end
-
-  def remove_job(job_id)
-    self.more_attributes['jobs'].delete(job_id)
-    self.save!
-  end
-
-  def jobs_completed?
-    self.more_attributes['jobs'].size == 0 rescue true
   end
 
   def scaling_workload
